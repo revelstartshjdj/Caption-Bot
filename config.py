f28456a161f976788c1ebf53b1fb4d4573e732e9
revelstartshjdj <rebelstarts0373@gmail.com>
2025-02-24 18:26:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 
 class Config(object):
       BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
+      API_ID = int(os.environ.get("26162072", 12345))
+      API_HASH = os.environ.get("ba25181c01b50d945748801b6c8b6ecc")
       CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
-      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "AvishkarPatil")
+      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Stylish_Star2")
